b81058a49be4741a7a94bb1ec699fb837deea84c
luuke <460035+luuke@users.noreply.github.com>
2023-01-31 08:55:27 +0100

    Add usart2 configuration

--- src/main.c
+++ src/main.c
@@ -1,12 +1,26 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
 
 #define SLEEP_TIME_MS ( 250 )
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 
+/* change this to any other UART peripheral if desired */
+#define UART_DEVICE_NODE DT_ALIAS(kissusart)
+
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct device *const uart_dev = DEVICE_DT_GET(UART_DEVICE_NODE);
+
+void print_uart(char *buf)
+{
+    int msg_len = strlen(buf);
+
+    for (int i = 0; i < msg_len; i++) {
+        uart_poll_out(uart_dev, buf[i]);
+    }
+}
 
 void main(void)
 {
@@ -22,6 +36,14 @@ void main(void)
     {
         return;
     }
+
+    if (!device_is_ready(uart_dev))
+    {
+        printk("UART device not found!");
+        return;
+    }
+
+    print_uart("Hello! I'm libcsp zephyr application.\r\n");
 
     while (1)
     {
