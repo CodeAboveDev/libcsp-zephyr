0d982f32d04d81d0eb9b407634269dfba210df90
luuke <460035+luuke@users.noreply.github.com>
2023-02-02 13:08:16 +0100

    Add uart polling tx functionality

--- src/usart_zephyr.c
+++ src/usart_zephyr.c
@@ -102,13 +102,14 @@ static void usart_rx_thread(void * p1, void * p2, void * p3)
 
 int csp_usart_write(csp_usart_fd_t fd, const void * data, size_t data_length)
 {
-    // if (fd >= 0) {
-    // 	int res = write(fd, data, data_length);
-    // 	if (res >= 0) {
-    // 		return res;
-    // 	}
-    // }
-    return CSP_ERR_TX;  // best matching CSP error code.
+    const unsigned char* tx_buf = data;
+    int i = 0;
+    for(int i = 0; i < data_length; i++)
+    {
+        uart_poll_out(csp_kiss_uart_dev, tx_buf[i]);
+    }
+
+    return i;
 }
 
 int csp_usart_open(const csp_usart_conf_t * conf, csp_usart_callback_t rx_callback, void * user_data, csp_usart_fd_t * return_fd)
