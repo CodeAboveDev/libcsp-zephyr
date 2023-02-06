6bd87f6f5b2a8f059c0656428d0a4788b5404ff1
luuke <460035+luuke@users.noreply.github.com>
2023-02-06 15:49:41 +0100

    Change UART polling to async

--- src/usart_zephyr.c
+++ src/usart_zephyr.c
@@ -7,22 +7,26 @@
 
 #include <stdlib.h>
 
-#define RX_THREAD_STACK_SIZE ( 800 )
-#define RX_THREAD_PRIORITY ( 0 )
+// #define RX_THREAD_STACK_SIZE ( 800 )
+// #define RX_THREAD_PRIORITY ( 0 )
 
+// TODO: Set buffer size using KISS interface MTU value
 #define RX_BUFFER_SIZE ( 200 )
 #define RX_RETRY ( 250 )
+#define RX_TIMEOUT ( 50000 )
 
-K_THREAD_STACK_DEFINE(rx_thread_stack_area, RX_THREAD_STACK_SIZE);
+// K_THREAD_STACK_DEFINE(rx_thread_stack_area, RX_THREAD_STACK_SIZE);
 
 static const struct device *const csp_kiss_uart_dev = DEVICE_DT_GET(DT_ALIAS(cspkissuart));
+
+static uint8_t rx_buffer[RX_BUFFER_SIZE];
 
 typedef struct {
     csp_usart_callback_t rx_callback;
     void * user_data;
     csp_usart_fd_t fd; // TODO: For Zephyr change underlying type to struct device pointer?
-    struct k_thread rx_thread;
-    k_tid_t rx_thread_id;
+    // struct k_thread rx_thread;
+    // k_tid_t rx_thread_id;
 } usart_context_t;
 
 K_MUTEX_DEFINE(lock);
@@ -37,79 +41,116 @@ void csp_usart_unlock(void * driver_data)
     k_mutex_unlock(&lock);
 }
 
-static void usart_rx_thread(void * p1, void * p2, void * p3)
-{
-	// ARG_UNUSED(p1);
-	ARG_UNUSED(p2);
-	ARG_UNUSED(p3);
+// static void usart_rx_thread(void * p1, void * p2, void * p3)
+// {
+// 	// ARG_UNUSED(p1);
+// 	ARG_UNUSED(p2);
+// 	ARG_UNUSED(p3);
 
-    usart_context_t * ctx = p1;
-    int ret;
-    uint8_t c;
-    int length = 0;
-    char rx_buf[RX_BUFFER_SIZE] = { 0 };
-    uint8_t retry = 0;
+//     usart_context_t * ctx = p1;
+//     int ret;
+//     uint8_t c;
+//     int length = 0;
+//     char rx_buf[RX_BUFFER_SIZE] = { 0 };
+//     uint8_t retry = 0;
 
-    while(1)
-    {
-        ret = uart_poll_in(csp_kiss_uart_dev, rx_buf+length);
+//     while(1)
+//     {
+//         ret = uart_poll_in(csp_kiss_uart_dev, rx_buf+length);
 
-        if(ret == 0 && length < RX_BUFFER_SIZE)
-        {
-            // csp_print("%c", c);
+//         if(ret == 0 && length < RX_BUFFER_SIZE)
+//         {
+//             // csp_print("%c", c);
 
-            retry = 0;
-            length++;
-        }
-        else if(ret == -1)
-        {
-            retry++;
-            if(retry >= RX_RETRY)
-            {
-                if(length > 0)
-                {
-                    csp_print("Len: %d, buf: %s\n", length, rx_buf);
-                    ctx->rx_callback(ctx->user_data, rx_buf, length, NULL);
+//             retry = 0;
+//             length++;
+//         }
+//         else if(ret == -1)
+//         {
+//             retry++;
+//             if(retry >= RX_RETRY)
+//             {
+//                 if(length > 0)
+//                 {
+//                     csp_print("Len: %d, buf: %s\n", length, rx_buf);
+//                     ctx->rx_callback(ctx->user_data, rx_buf, length, NULL);
 
-                    length = 0;
-                    k_msleep(10);
-                }
-                else
-                {
-                    // csp_print("Len: %d\n", length);
-                }
-                retry = 0;
+//                     length = 0;
+//                     k_msleep(10);
+//                 }
+//                 else
+//                 {
+//                     // csp_print("Len: %d\n", length);
+//                 }
+//                 retry = 0;
 
-                // k_msleep(10);
-            }
-        }
-    }
+//                 // k_msleep(10);
+//             }
+//         }
+//     }
 
-    // usart_context_t * ctx = arg;
-    // const unsigned int CBUF_SIZE = 400;
-    // uint8_t * cbuf = malloc(CBUF_SIZE);
+//     // usart_context_t * ctx = arg;
+//     // const unsigned int CBUF_SIZE = 400;
+//     // uint8_t * cbuf = malloc(CBUF_SIZE);
 
-    // // Receive loop
-    // while (1) {
-    //     int length = read(ctx->fd, cbuf, CBUF_SIZE);
-    //     if (length <= 0) {
-    //         csp_print("%s: read() failed, returned: %d\n", __FUNCTION__, length);
-    //         exit(1);
-    //     }
-    //     ctx->rx_callback(ctx->user_data, cbuf, length, NULL);
-    // }
-}
+//     // // Receive loop
+//     // while (1) {
+//     //     int length = read(ctx->fd, cbuf, CBUF_SIZE);
+//     //     if (length <= 0) {
+//     //         csp_print("%s: read() failed, returned: %d\n", __FUNCTION__, length);
+//     //         exit(1);
+//     //     }
+//     //     ctx->rx_callback(ctx->user_data, cbuf, length, NULL);
+//     // }
+// }
 
 int csp_usart_write(csp_usart_fd_t fd, const void * data, size_t data_length)
 {
-    const unsigned char* tx_buf = data;
-    int i = 0;
-    for(int i = 0; i < data_length; i++)
+    // Timeout in microseconds valid only if flow control is enabled
+    csp_print("csp_usart_write: %d\n", data_length);
+    int ret = uart_tx(csp_kiss_uart_dev, data, data_length, SYS_FOREVER_US);
+    if(ret != 0)
     {
-        uart_poll_out(csp_kiss_uart_dev, tx_buf[i]);
+        return ret;
     }
 
-    return i;
+    return data_length;
+}
+
+void csp_kiss_uart_event_cb(const struct device* dev, struct uart_event* evt, void* user_data)
+{
+    csp_print("uart event: %d\n", evt->type);
+    usart_context_t * ctx = user_data;
+
+    switch(evt->type)
+    {
+    case UART_TX_DONE:
+        csp_print("tx: %d\n", evt->data.tx.len);
+        break;
+
+    case UART_TX_ABORTED:
+        break;
+
+	case UART_RX_RDY:
+        uart_rx_disable(csp_kiss_uart_dev);
+        csp_print("rx: %d\n", evt->data.rx.len);
+        ctx->rx_callback(ctx->user_data, evt->data.rx.buf+evt->data.rx.offset, evt->data.rx.len, NULL);
+        break;
+
+    case UART_RX_BUF_REQUEST:
+        break;
+
+	case UART_RX_BUF_RELEASED:
+        break;
+
+	case UART_RX_DISABLED:
+        uart_rx_enable(csp_kiss_uart_dev, rx_buffer, RX_BUFFER_SIZE, RX_TIMEOUT);
+        break;
+
+	case UART_RX_STOPPED:
+        break;
+
+    }
 }
 
 int csp_usart_open(const csp_usart_conf_t * conf, csp_usart_callback_t rx_callback, void * user_data, csp_usart_fd_t * return_fd)
@@ -143,7 +184,7 @@ int csp_usart_open(const csp_usart_conf_t * conf, csp_usart_callback_t rx_callba
     usart_context_t * ctx = calloc(1, sizeof(*ctx));
     if (ctx == NULL)
     {
-        csp_print("%s: Error allocating context, device: [%s], errno: %s\n", __FUNCTION__, conf->device, strerror(errno));
+        csp_print("%s: Error allocating context, device: [%s], errno: %s\n", __FUNCTION__, csp_kiss_uart_dev->name, strerror(errno));
         return CSP_ERR_NOMEM;
     }
     ctx->rx_callback = rx_callback;
@@ -153,11 +194,27 @@ int csp_usart_open(const csp_usart_conf_t * conf, csp_usart_callback_t rx_callba
     // Spawn RX thread
     if (rx_callback)
     {
+        int ret = uart_callback_set(csp_kiss_uart_dev, csp_kiss_uart_event_cb, ctx);
+        if(ret != 0)
+        {
+            csp_print("Error setting UART callback, device: [%s], return: %d\n", csp_kiss_uart_dev->name, ret);
+
+            return CSP_ERR_DRIVER;
+        }
+
+        ret = uart_rx_enable(csp_kiss_uart_dev, rx_buffer, RX_BUFFER_SIZE, RX_TIMEOUT);
+        if(ret != 0)
+        {
+            csp_print("Error enabling UART RX, device: [%s], return: %d (%s)\n", csp_kiss_uart_dev->name, ret, strerror(-ret));
+
+            return CSP_ERR_DRIVER;
+        }
+        
         // TODO: How about creating thread at compile time and only enabling it here?
-        ctx->rx_thread_id = k_thread_create(&ctx->rx_thread, rx_thread_stack_area,
-            K_THREAD_STACK_SIZEOF(rx_thread_stack_area), usart_rx_thread,
-            ctx, NULL, NULL,
-            RX_THREAD_PRIORITY, 0, K_NO_WAIT);
+        // ctx->rx_thread_id = k_thread_create(&ctx->rx_thread, rx_thread_stack_area,
+        //     K_THREAD_STACK_SIZEOF(rx_thread_stack_area), usart_rx_thread,
+        //     ctx, NULL, NULL,
+        //     RX_THREAD_PRIORITY, 0, K_NO_WAIT);
 
         // if(is there anything that can go wrong?)
         // {
