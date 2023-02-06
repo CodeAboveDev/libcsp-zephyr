5534d45be23a8c00c82a2f02f92cc7fa31c7e3fd
luuke <460035+luuke@users.noreply.github.com>
2023-02-06 16:09:35 +0100

    Some code cleanup

--- src/usart_zephyr.c
+++ src/usart_zephyr.c
@@ -7,15 +7,9 @@
 
 #include <stdlib.h>
 
-// #define RX_THREAD_STACK_SIZE ( 800 )
-// #define RX_THREAD_PRIORITY ( 0 )
-
 // TODO: Set buffer size using KISS interface MTU value
 #define RX_BUFFER_SIZE ( 200 )
-#define RX_RETRY ( 250 )
-#define RX_TIMEOUT ( 50000 )
-
-// K_THREAD_STACK_DEFINE(rx_thread_stack_area, RX_THREAD_STACK_SIZE);
+#define RX_TIMEOUT_US ( 50000 )
 
 static const struct device *const csp_kiss_uart_dev = DEVICE_DT_GET(DT_ALIAS(cspkissuart));
 
@@ -25,8 +19,6 @@ typedef struct {
     csp_usart_callback_t rx_callback;
     void * user_data;
     csp_usart_fd_t fd; // TODO: For Zephyr change underlying type to struct device pointer?
-    // struct k_thread rx_thread;
-    // k_tid_t rx_thread_id;
 } usart_context_t;
 
 K_MUTEX_DEFINE(lock);
@@ -40,69 +32,6 @@ void csp_usart_unlock(void * driver_data)
 {
     k_mutex_unlock(&lock);
 }
-
-// static void usart_rx_thread(void * p1, void * p2, void * p3)
-// {
-// 	// ARG_UNUSED(p1);
-// 	ARG_UNUSED(p2);
-// 	ARG_UNUSED(p3);
-
-//     usart_context_t * ctx = p1;
-//     int ret;
-//     uint8_t c;
-//     int length = 0;
-//     char rx_buf[RX_BUFFER_SIZE] = { 0 };
-//     uint8_t retry = 0;
-
-//     while(1)
-//     {
-//         ret = uart_poll_in(csp_kiss_uart_dev, rx_buf+length);
-
-//         if(ret == 0 && length < RX_BUFFER_SIZE)
-//         {
-//             // csp_print("%c", c);
-
-//             retry = 0;
-//             length++;
-//         }
-//         else if(ret == -1)
-//         {
-//             retry++;
-//             if(retry >= RX_RETRY)
-//             {
-//                 if(length > 0)
-//                 {
-//                     csp_print("Len: %d, buf: %s\n", length, rx_buf);
-//                     ctx->rx_callback(ctx->user_data, rx_buf, length, NULL);
-
-//                     length = 0;
-//                     k_msleep(10);
-//                 }
-//                 else
-//                 {
-//                     // csp_print("Len: %d\n", length);
-//                 }
-//                 retry = 0;
-
-//                 // k_msleep(10);
-//             }
-//         }
-//     }
-
-//     // usart_context_t * ctx = arg;
-//     // const unsigned int CBUF_SIZE = 400;
-//     // uint8_t * cbuf = malloc(CBUF_SIZE);
-
-//     // // Receive loop
-//     // while (1) {
-//     //     int length = read(ctx->fd, cbuf, CBUF_SIZE);
-//     //     if (length <= 0) {
-//     //         csp_print("%s: read() failed, returned: %d\n", __FUNCTION__, length);
-//     //         exit(1);
-//     //     }
-//     //     ctx->rx_callback(ctx->user_data, cbuf, length, NULL);
-//     // }
-// }
 
 int csp_usart_write(csp_usart_fd_t fd, const void * data, size_t data_length)
 {
@@ -144,7 +73,7 @@ void csp_kiss_uart_event_cb(const struct device* dev, struct uart_event* evt, vo
         break;
 
 	case UART_RX_DISABLED:
-        uart_rx_enable(csp_kiss_uart_dev, rx_buffer, RX_BUFFER_SIZE, RX_TIMEOUT);
+        uart_rx_enable(csp_kiss_uart_dev, rx_buffer, RX_BUFFER_SIZE, RX_TIMEOUT_US);
         break;
 
 	case UART_RX_STOPPED:
@@ -164,22 +93,6 @@ int csp_usart_open(const csp_usart_conf_t * conf, csp_usart_callback_t rx_callba
         return CSP_ERR_DRIVER;
     }
 
-    // Configure polling/interrupts/events here
-    // For now do nothing, but keep the commented code here
-
-    // ret = uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL);
-    // if (ret < 0) {
-    //     if (ret == -ENOTSUP) {
-    //         printk("Interrupt-driven UART API support not enabled\n");
-    //     } else if (ret == -ENOSYS) {
-    //         printk("UART device does not support interrupt-driven API\n");
-    //     } else {
-    //         printk("Error setting UART callback: %d\n", ret);
-    //     }
-    //     return;
-    // }
-    // uart_irq_rx_enable(uart_dev);
-
     // Allocate context memory
     usart_context_t * ctx = calloc(1, sizeof(*ctx));
     if (ctx == NULL)
@@ -192,40 +105,28 @@ int csp_usart_open(const csp_usart_conf_t * conf, csp_usart_callback_t rx_callba
     // ctx->fd = fd; // TODO: Read struct member comment
 
     // Setup UART callback
-        int ret = uart_callback_set(csp_kiss_uart_dev, csp_kiss_uart_event_cb, ctx);
-        if(ret != 0)
-        {
-            csp_print("Error setting UART callback, device: [%s], return: %d\n", csp_kiss_uart_dev->name, ret);
+    int ret = uart_callback_set(csp_kiss_uart_dev, csp_kiss_uart_event_cb, ctx);
+    if(ret != 0)
+    {
+        csp_print("Error setting UART callback, device: [%s], return: %d\n", csp_kiss_uart_dev->name, ret);
 
-            return CSP_ERR_DRIVER;
-        }
+        return CSP_ERR_DRIVER;
+    }
 
     // Enable RX if callback provided
     if (rx_callback)
     {
-        ret = uart_rx_enable(csp_kiss_uart_dev, rx_buffer, RX_BUFFER_SIZE, RX_TIMEOUT);
+        ret = uart_rx_enable(csp_kiss_uart_dev, rx_buffer, RX_BUFFER_SIZE, RX_TIMEOUT_US);
         if(ret != 0)
         {
             csp_print("Error enabling UART RX, device: [%s], return: %d (%s)\n", csp_kiss_uart_dev->name, ret, strerror(-ret));
 
             return CSP_ERR_DRIVER;
         }
-        
-        // TODO: How about creating thread at compile time and only enabling it here?
-        // ctx->rx_thread_id = k_thread_create(&ctx->rx_thread, rx_thread_stack_area,
-        //     K_THREAD_STACK_SIZEOF(rx_thread_stack_area), usart_rx_thread,
-        //     ctx, NULL, NULL,
-        //     RX_THREAD_PRIORITY, 0, K_NO_WAIT);
-
-        // if(is there anything that can go wrong?)
-        // {
-        //     csp_print("%s: k_thread_create() failed to create Rx thread for device: [%s], errno: %s\n", __FUNCTION__, conf->device, strerror(errno));
-        //     free(ctx);
-        //     return CSP_ERR_NOMEM;
-        // }
     }
 
-    if (return_fd) {
+    if (return_fd)
+    {
         // *return_fd = fd; // TODO: Read struct member comment
     }
 
